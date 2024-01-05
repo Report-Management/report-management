324318508ca4db574895cbc4c7450985e8b1d467
Phanith-LIM <limphanith.dev@gmail.com>
2024-01-05 23:45:04 +0700

    [PROD]: Deploy 3.3

--- src/pages/admin/dashboard/page.jsx
+++ src/pages/admin/dashboard/page.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BarChart, PieChart } from "../../../components/chart";
+import { BarChart, PieChart } from "../../../components/chart/index.jsx";
 import { Card } from "flowbite-react";
 import { DashboardRepository } from "./repository.js";
 import { Loading } from "../../../components/index.jsx";
@@ -10,21 +10,20 @@ export class AdminDashboardView extends Component {
     loading: true,
   };
 
-  getReport = async (year) => {
+  async getReport(year) {
     const dashboardRepository = new DashboardRepository();
     let result = null;
     if (year === 0) {
       result = await dashboardRepository.getReportYear();
-    }
-    else {
+    } else {
       result = await dashboardRepository.getReportMonth(year);
     }
     if (result !== null) {
-      this.setState({ dataReportMonth: result });
+      this.setState({ dataReportMonth: result }, this.checkData);
     }
-  };
+  }
 
-  getReportCategory = async (year) => {
+  async getReportCategory(year) {
     const dashboardRepository = new DashboardRepository();
     let result = null;
     if (year === 0) {
@@ -33,35 +32,35 @@ export class AdminDashboardView extends Component {
       result = await dashboardRepository.getReportCategoryYear(year);
     }
     if (result !== null) {
-      this.setState({ dataReportCategoryYear: result });
+      this.setState({ dataReportCategoryYear: result }, this.checkData);
     }
   }
 
-  getReportSolve = async (year, month) => {
+  async getReportSolve(year, month) {
     const dashboardRepository = new DashboardRepository();
-    const result = await dashboardRepository.getReportSolve(year, this.convert_months(month));
+    const result = await dashboardRepository.getReportSolve(year, this.convertMonths(month));
     if (result !== null) {
-      this.setState({ dataReportSolve: result });
+      this.setState({ dataReportSolve: result }, this.checkData);
     }
   }
 
-  getReportSpam = async (year, month) => {
+  async getReportSpam(year, month) {
     const dashboardRepository = new DashboardRepository();
-    const result = await dashboardRepository.getReportSpam(year, this.convert_months(month));
+    const result = await dashboardRepository.getReportSpam(year, this.convertMonths(month));
     if (result !== null) {
-      this.setState({ dataReportSpam: result });
+      this.setState({ dataReportSpam: result }, this.checkData);
     }
   }
 
-  getReportDetail = async () => {
+  async getReportDetail() {
     const dashboardRepository = new DashboardRepository();
     const result = await dashboardRepository.getReportDetail();
     if (result !== null) {
-      this.setState({ dataReportDetail: result });
+      this.setState({ dataReportDetail: result }, this.checkData);
     }
   }
 
-  getDate = async () => {
+  async getDate() {
     const dashboardRepository = new DashboardRepository();
     const result = await dashboardRepository.getDate();
     if (result !== null) {
@@ -82,6 +81,8 @@ export class AdminDashboardView extends Component {
         pie2YearLabel: 0,
         pie2Month: result.thisMonth,
         pie2MonthLabel: "All",
+      }, () => {
+        this.getData();
       });
     }
   }
@@ -96,7 +97,7 @@ export class AdminDashboardView extends Component {
     await this.getReportCategory(item);
   }
 
-  handlepie1Year = async (item) => {
+  handlePie1Year = async (item) => {
     if (item === 0 && this.state.pie1MonthLabel !== 0) {
       this.setState({ pie1MonthLabel: "All" });
     }
@@ -104,18 +105,18 @@ export class AdminDashboardView extends Component {
       this.setState({ pie1Month: this.state.thisMonth });
       this.setState({ pie1MonthLabel: "All" });
     } else {
-      this.setState({ pie1Month: this.state.allMonth })
+      this.setState({ pie1Month: this.state.allMonth });
     }
     this.setState({ pie1YearLabel: item });
     await this.getReportSolve(item, this.state.pie1MonthLabel);
   }
 
-  handlepie1Month = async (item) => {
+  handlePie1Month = async (item) => {
     this.setState({ pie1MonthLabel: item });
     await this.getReportSolve(this.state.pie1YearLabel, item);
   }
 
-  handlepie2Year = async (item) => {
+  handlePie2Year = async (item) => {
     if (item === 0 && this.state.pie2MonthLabel !== 0) {
       this.setState({ pie2MonthLabel: "All" });
     }
@@ -123,53 +124,75 @@ export class AdminDashboardView extends Component {
       this.setState({ pie2Month: this.state.thisMonth });
       this.setState({ pie2MonthLabel: "All" });
     } else {
-      this.setState({ pie2Month: this.state.allMonth })
+      this.setState({ pie2Month: this.state.allMonth });
     }
     this.setState({ pie2YearLabel: item });
     await this.getReportSpam(item, this.state.pie2MonthLabel);
   }
 
-  handlepie2Month = async (item) => {
+  handlePie2Month = async (item) => {
     this.setState({ pie2MonthLabel: item });
     await this.getReportSpam(this.state.pie2YearLabel, item);
   }
 
   checkData = () => {
-    if (this.state.dataReportMonth === undefined
-        || this.state.dataReportCategoryYear === undefined
-        || this.state.dataReportSolve === undefined
-        || this.state.dataReportSpam === undefined
-        || this.state.dataReportDetail === undefined) {
+    const {
+      dataReportMonth,
+      dataReportCategoryYear,
+      dataReportSolve,
+      dataReportSpam,
+      dataReportDetail
+    } = this.state;
+
+    if (
+        dataReportMonth === undefined ||
+        dataReportCategoryYear === undefined ||
+        dataReportSolve === undefined ||
+        dataReportSpam === undefined ||
+        dataReportDetail === undefined
+    ) {
       this.setState({ loading: true });
     } else {
       this.setState({ loading: false });
     }
   }
 
-  convert_months(month){
-    const months = {"All":0, "Jan":1, "Feb":2, "Mar":3, "Apr":4, "May":5, "Jun":6,"Jul":7, "Aug":8, "Sep":9, "Oct":10, "Nov":11, "Dec":12}
+  convertMonths(month) {
+    const months = {
+      "All": 0,
+      "Jan": 1,
+      "Feb": 2,
+      "Mar": 3,
+      "Apr": 4,
+      "May": 5,
+      "Jun": 6,
+      "Jul": 7,
+      "Aug": 8,
+      "Sep": 9,
+      "Oct": 10,
+      "Nov": 11,
+      "Dec": 12
+    }
     return months[month]
   }
 
-  getData = async () => {
-    await this.getDate();
-    await this.getReport(this.state.bar1Label);
-    await this.getReportCategory(this.state.bar2Label);
-    await this.getReportSolve(this.state.pie1YearLabel, this.state.pie1MonthLabel);
-    await this.getReportSpam(this.state.pie2YearLabel, this.state.pie2MonthLabel);
-    await this.getReportDetail();
-    await this.checkData();
+  getData() {
+    this.getReport(this.state.bar1Label);
+    this.getReportCategory(this.state.bar2Label);
+    this.getReportSolve(this.state.pie1YearLabel, this.state.pie1MonthLabel);
+    this.getReportSpam(this.state.pie2YearLabel, this.state.pie2MonthLabel);
+    this.getReportDetail();
   }
 
   async componentDidMount() {
-    await this.getData();
+    await this.getDate();
   }
 
   render() {
     const { loading, dataReportMonth, dataReportCategoryYear, dataReportSolve, dataReportSpam, dataReportDetail } = this.state;
 
     if (loading) {
-      return <Loading /> // Render a loading message or spinner
+      return <Loading />; // Render a loading message or spinner
     }
 
     return (
@@ -178,10 +201,12 @@ export class AdminDashboardView extends Component {
             <div className="w-full">
               <div className="flex flex-row space-x-3 w-full">
                 <div className="flex-auto w-1/2">
-                  <div className="rounded-xl bg-white dark:bg-gray-900 drop-shadow-md w-full" >
+                  <div className="rounded-xl bg-white dark:bg-gray-900 drop-shadow-md w-full">
                     <Dropdown label={this.state.bar1Label === 0 ? "All Year" : this.state.bar1Label} dismissOnClick={false} color="light">
-                      {this.state.bar1.map((item, index) => (
-                          <Dropdown.Item key={index} onClick={() => this.handleBar1(item)}>{item === 0 ? "All Year" : item}</Dropdown.Item>
+                      {this.state.bar1.map(item => (
+                          <Dropdown.Item key={item} onClick={() => this.handleBar1(item)}>
+                            {item === 0 ? "All Year" : item}
+                          </Dropdown.Item>
                       ))}
                     </Dropdown>
 
@@ -190,14 +215,15 @@ export class AdminDashboardView extends Component {
                         xLabels={dataReportMonth.xLabels}
                         datasets={dataReportMonth.datasets}
                     />
-
                   </div>
                 </div>
                 <div className="flex-auto w-1/2">
                   <div className="rounded-xl bg-white dark:bg-gray-900 drop-shadow-md">
                     <Dropdown label={this.state.bar2Label === 0 ? "All" : this.state.bar2Label} dismissOnClick={false} color="light">
-                      {this.state.bar2.map((item, index) => (
-                          <Dropdown.Item key={index} onClick={() => this.handleBar2(item)}>{item === 0 ? "All" : item}</Dropdown.Item>
+                      {this.state.bar2.map(item => (
+                          <Dropdown.Item key={item} onClick={() => this.handleBar2(item)}>
+                            {item === 0 ? "All" : item}
+                          </Dropdown.Item>
                       ))}
                     </Dropdown>
                     <BarChart
@@ -212,20 +238,23 @@ export class AdminDashboardView extends Component {
 
             <div className="w-full h-full">
               <div className="flex justify-center items-center h-full">
-
                 <div className="flex-auto w-1/2 h-full pt-8">
                   <div className="h-full">
                     <div className="flex h-full">
                       <div className="rounded-xl bg-white dark:bg-gray-900 drop-shadow-md m-2">
                         <div className="flex-auto h-full" style={{ flexBasis: '50%' }}>
                           <Dropdown label={this.state.pie1YearLabel === 0 ? "All" : this.state.pie1YearLabel} dismissOnClick={false} color="light">
-                            {this.state.pie1Year.map((item, index) => (
-                                <Dropdown.Item key={index} onClick={() => this.handlepie1Year(item)}>{item === 0 ? "All" : item}</Dropdown.Item>
+                            {this.state.pie1Year.map(item => (
+                                <Dropdown.Item key={item} onClick={() => this.handlePie1Year(item)}>
+                                  {item === 0 ? "All" : item}
+                                </Dropdown.Item>
                             ))}
                           </Dropdown>
                           <Dropdown label={this.state.pie1MonthLabel} dismissOnClick={false} color="light">
-                            {this.state.pie1Month.map((item, index) => (
-                                <Dropdown.Item key={index} onClick={() => this.handlepie1Month(item)}>{item}</Dropdown.Item>
+                            {this.state.pie1Month.map(item => (
+                                <Dropdown.Item key={item} onClick={() => this.handlePie1Month(item)}>
+                                  {item}
+                                </Dropdown.Item>
                             ))}
                           </Dropdown>
                           <PieChart
@@ -238,13 +267,17 @@ export class AdminDashboardView extends Component {
                       <div className="rounded-xl bg-white dark:bg-gray-900 drop-shadow-md m-2">
                         <div className="flex-auto h-full" style={{ flexBasis: '50%' }}>
                           <Dropdown label={this.state.pie2YearLabel === 0 ? "All" : this.state.pie2YearLabel} dismissOnClick={false} color="light">
-                            {this.state.pie2Year.map((item, index) => (
-                                <Dropdown.Item key={index} onClick={() => this.handlepie2Year(item)}>{item === 0 ? "All" : item}</Dropdown.Item>
+                            {this.state.pie2Year.map(item => (
+                                <Dropdown.Item key={item} onClick={() => this.handlePie2Year(item)}>
+                                  {item === 0 ? "All" : item}
+                                </Dropdown.Item>
                             ))}
                           </Dropdown>
                           <Dropdown label={this.state.pie2MonthLabel} dismissOnClick={false} color="light">
-                            {this.state.pie2Month.map((item, index) => (
-                                <Dropdown.Item key={index} onClick={() => this.handlepie2Month(item)}>{item}</Dropdown.Item>
+                            {this.state.pie2Month.map(item => (
+                                <Dropdown.Item key={item} onClick={() => this.handlePie2Month(item)}>
+                                  {item}
+                                </Dropdown.Item>
                             ))}
                           </Dropdown>
                           <PieChart
@@ -257,7 +290,6 @@ export class AdminDashboardView extends Component {
                     </div>
                   </div>
                 </div>
-
                 <div className="flex-auto w-1/8 h-full">
                   <div className="h-full">
                     <div className="grid grid-cols-2 gap-3 h-full">
@@ -265,8 +297,7 @@ export class AdminDashboardView extends Component {
                           <div key={index} className="h-full flex items-center justify-center">
                             <Card className="w-full h-full flex flex-col justify-center items-center">
                               <div className="text-gray-400 font-nunito font-bold">{item.label}</div>
-                              <div
-                                  className="text-4xl font-bold font-mono text-purple-800">{item.number}</div>
+                              <div className="text-4xl font-bold font-mono text-purple-800">{item.number}</div>
                             </Card>
                           </div>
                       ))}
