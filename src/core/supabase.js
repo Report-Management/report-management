484878a5d1770d1be1eecc131bd89e5eee740504
limphanith <limphanith.dev@gmail.com>
2024-07-16 15:21:40 +0700

    Fix README.md

--- src/core/supabase.js
+++ src/core/supabase.js
@@ -1,6 +1,6 @@
 import {createClient} from "@supabase/supabase-js";
 
 export const supabaseSession = createClient(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_ANON_KEY,
+    "https://ynnxablyssvpuufbvgxq.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlubnhhYmx5c3N2cHV1ZmJ2Z3hxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMTExMjY4NCwiZXhwIjoyMDM2Njg4Njg0fQ.12N-bHeI81IS17HTuBZoqXalY6jTYmNogv_0y-Pq_Kc"
 )
